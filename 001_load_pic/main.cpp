8817fd097b5d1fd654ffc1739be431ac567d1cce
Arthur Yii <841158840@qq.com>
2014-03-05 13:53:09 +0800

    first project to load a img

---
+++ 001_load_pic/main.cpp
@@ -0,0 +1,11 @@
+#include <opencv.hpp>
+using namespace cv;
+int main()
+{
+	IplImage* img = cvLoadImage("lena.jpg");
+	cvNamedWindow("Example1",CV_WINDOW_AUTOSIZE );
+	cvShowImage("Example1", img );
+	cvWaitKey(0);
+	cvReleaseImage( &img );
+	cvDestroyWindow("Example1");
+}
